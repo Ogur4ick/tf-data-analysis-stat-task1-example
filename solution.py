e8dbbfd5847dd9114b70c704e5bac77bd9b883af
Ogur4ick <75222531+Ogur4ick@users.noreply.github.com>
2023-04-07 05:20:43 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,7 +5,9 @@ import numpy as np
 chat_id =  968976840 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
+    n = 100  
+    t = 41  
+    s = np.random.laplace(loc=100, scale=10, size=n)  
+    a = 2 * s / (t ** 2)  
+    x_mean = np.mean(a)  
     return x.mean() # Ваш ответ
